60947c72a5caff462d2a6e4f6aedc77998dd01bb
ContentCoder <codingforcontent@gmail.com>
2014-03-27 22:35:34 +0800

    set charset=utf-8

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ function getQuotes(req, res) {
 }
 
 function responseJSON(res, statusCode, msg) {
-  res.writeHead(statusCode, {'Content-Type': 'application/json'});
+  res.writeHead(statusCode, {'Content-Type': 'application/json; charset=utf-8'});
   res.end(JSON.stringify(msg));
   util.log(JSON.stringify(msg));
   return;
